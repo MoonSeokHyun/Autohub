9c98006f9b177760e5f2ee6b701ebb51124a9b86
문석현 <munseoghyeon@Mac.wlan>
2024-10-30 01:08:55 +0900

    s

--- app/Views/gas_station/detail.php
+++ app/Views/gas_station/detail.php
@@ -323,7 +323,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     <footer style="background-color: #f8f9fa; padding: 20px; text-align: center; font-size: 14px; color: #6c757d;">
     <p>본 데이터는 <a href="https://www.data.go.kr" target="_blank" style="color: #007bff; text-decoration: none;">www.data.go.kr</a>에서 데이터 기반으로 만들어진 웹 사이트입니다.</p>
     <p>이 웹 사이트는 영리 목적으로 만들어진 사이트입니다.</p>
-    <p>잘못된 정보는 <a href="mailto:mls0000@naver.com" style="color: #007bff; text-decoration: none;">mls0000@naver.com</a>으로 문의해 주세요.</p>
+    <p>잘못된 정보는 <a href="mailto:gjqmaoslwj@naver.com" style="color: #007bff; text-decoration: none;">gjqmaoslwj@naver.com</a>으로 문의해 주세요.</p>
 </footer>
 
     <script>
